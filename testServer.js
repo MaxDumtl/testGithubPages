a6689bb69ce4c180671431971df93414eca1d344
MaxDumtl <45670566+MaxDumtl@users.noreply.github.com>
2020-06-24 10:26:57 +0200

    delete testServer.js

--- testServer.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");  
-var server = http.createServer(function(request, response) {  
-    response.writeHead(200, {  
-        'Content-Type': 'text/plain'  
-    });  
-    response.write("This is Test Message.");  
-    response.end();  
-});  
-server.listen(8082); 
